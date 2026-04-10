3d2595a0bd77455784955044cb8eef44695b65cf
GlobalZon <benjaminadzessa@gmail.com>
2026-04-10 01:38:40 +0100

    fix(render): Revert strict DEBUG and ALLOWED_HOSTS to fix Error 1 on deployment

--- proph_couture_project/proph_couture_project/settings.py
+++ proph_couture_project/proph_couture_project/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&947mzy1nsvbxfco#r*klis)g+cu!w^8as*xb&hgm(07dyfelg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'backend-1-7oti.onrender.com', 'prophetiecouture.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com', '*', 'prophetiecouture.vercel.app']
 
 # Application definition
 
